d465cadbd844d7121e6d829fb99cfd2d37a9e7f8
Янтимир Мингазитдинов <mingazitdinov@mail.ru>
2021-11-28 13:57:33 +0400

    Бургер заготовка стрелка вправо

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    IOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.IOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    }
+};
+/* код определяет на каком устройстве открыта страница*/
+
+// Меню бургер
+const menuBurger = document.querySelector('.menu-burger');
+
+if (menuBurger) {
+    menuBurger.addEventListener("click", function (e) {
+        menuBurger.classList.toggle('menu-burger_active');
+    });
+};
